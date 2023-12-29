24281aa166731ce623dbe09b0fe8b533c74020f9
Victor <victor.lopez@king.com>
2023-12-29 22:01:48 +0100

    Upgraded tools

--- thirdparty/piper_phonemize/setup.py
+++ thirdparty/piper_phonemize/setup.py
@@ -2,18 +2,18 @@ import platform
 from pathlib import Path
 
 import os
-
+import sys
+import platform
 
 
 # Available at setup time due to pyproject.toml
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
-_DIR = Path(os.environ['VIPER_ROOT'])
-_ESPEAK_DIR = _DIR / "install"
-_ONNXRUNTIME_DIR = _DIR / "artifacts"
+_PREFIX_PATH = Path(os.environ['VIPER_ROOT'])
 
 __version__ = "1.2.0"
+
 
 ext_modules = [
     Pybind11Extension(
@@ -25,26 +25,27 @@ ext_modules = [
             "src/tashkeel.cpp",
         ],
         define_macros=[("VERSION_INFO", __version__)],
-        include_dirs=[str(_ESPEAK_DIR / "include"), str(_ONNXRUNTIME_DIR / "include")],
-        library_dirs=[str(_ESPEAK_DIR / "lib"), str(_ONNXRUNTIME_DIR / "lib")],
-        libraries=["espeak-ng", "onnxruntime"],
+        include_dirs=[str(_PREFIX_PATH / "include")],
+        library_dirs=[str(_PREFIX_PATH / "lib")],
+        libraries=["espeak-ng", "ucd", "speechPlayer", "onnxruntime"],
+        extra_link_args=['-Wl,-rpath,' + str(_PREFIX_PATH / "lib")],
     ),
 ]
 
 setup(
     name="piper_phonemize",
     version=__version__,
-    author="Michael Hansen",
-    author_email="mike@rhasspy.org",
-    url="https://github.com/rhasspy/piper-phonemize",
-    description="Phonemization libary used by Piper text to speech system",
+    author="ShadedBandit",
+    author_email="animacion@greentwip.xyz",
+    url="https://github.com/Cybertwip/viper",
+    description="Phonemization libary used by Viper text to speech system",
     long_description="",
     packages=["piper_phonemize"],
     package_data={
         "piper_phonemize": [
-            str(p) for p in (_DIR / "data" / "espeak-ng-data").rglob("*")
+            str(p) for p in (_PREFIX_PATH / "share" / "espeak-ng-data").rglob("*")
         ]
-        + [str(_DIR / "data" / "libtashkeel_model.ort")]
+        + [str(_PREFIX_PATH / "share" / "libtashkeel_model.ort")]
     },
     include_package_data=True,
     ext_modules=ext_modules,
